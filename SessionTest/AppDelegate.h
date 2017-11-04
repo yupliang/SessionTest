7735534570cb13f24217e0a93af8323d2bc8704a
yupeiliang <yupeiliang@fotoable.com>
2017-11-04 16:49:49 +0800

    add Xcode proj.

---
+++ SessionTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SessionTest
+//
+//  Created by yupeiliang on 2017/11/4.
+//  Copyright © 2017年 PL Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SessionTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SessionTest
+//
+//  Created by yupeiliang on 2017/11/4.
+//  Copyright © 2017年 PL Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
