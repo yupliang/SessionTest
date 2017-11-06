2d6dd552fcfe734490c8c66150eb646206c21de7
yupeiliang <yupeiliang@fotoable.com>
2017-11-06 11:24:17 +0800

    添加测试用例。

--- SessionTestTests/SessionTestTests.m
+++ SessionTestTests/SessionTestTests.m
@@ -27,6 +27,19 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Request should succeed"];
+    NSURLSessionConfiguration *defaultConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *sessionWithoutADelegate = [NSURLSession sessionWithConfiguration:defaultConfiguration];
+    NSURL *url = [NSURL URLWithString:@"https://www.example.com/"];
+    __block BOOL flag = NO;
+    [[sessionWithoutADelegate dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSLog(@"Got response %@ with error %@.\n", response, error);
+        NSLog(@"DATA:\n%@\nEND DATA\n", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+        flag = true;
+        [expectation fulfill];
+  }] resume];
+    [self waitForExpectationsWithTimeout:10.0 handler:nil];
+    XCTAssertTrue(flag == true);
 }
 
 - (void)testPerformanceExample {
